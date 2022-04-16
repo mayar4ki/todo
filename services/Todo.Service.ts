37f856f6a8b0a44140a78401b6a1d90b42a3eac2
Mayar <chrollo4ki@gmail.com>
2022-04-16 19:35:48 +0300

    Update Todo.Service.ts

--- services/Todo.Service.ts
+++ services/Todo.Service.ts
@@ -1,5 +1,5 @@
 import { CreateTodo, Todo, UpdateTodo } from "@interfaces";
-import Axios, { AxiosObservable } from "axios-observable";
+import axios from "axios";
 
 const  {NEXT_PUBLIC_PROXY_URL} =process.env
 
@@ -19,24 +19,24 @@ class TodoService {
     }
 
 
-        Index(): AxiosObservable<Todo[]> {
-            return Axios.get<Todo[]>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}`);
+        Index() {
+            return axios.get<Todo[]>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}`);
         }
         
-        Show(id:string): AxiosObservable<Todo> {  
-              return  Axios.get<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}/${id}`);
+        Show(id:string) {  
+              return  axios.get<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}/${id}`);
         }
 
-        Store(data:CreateTodo): AxiosObservable<Todo> {
-            return Axios.post<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}`,data);
+        Store(data:CreateTodo) {
+            return axios.post<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}`,data);
         }
 
-        Update(id:string,data:UpdateTodo): AxiosObservable<Todo> {
-            return Axios.put<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}/${id}`,data);
+        Update(id:string,data:UpdateTodo) {
+            return axios.put<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}/${id}`,data);
         }
 
-        Destroy(id:string): AxiosObservable<Todo> {
-            return Axios.delete<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}/${id}`);
+        Destroy(id:string) {
+            return axios.delete<Todo>(`${NEXT_PUBLIC_PROXY_URL}/${this.ITEM}/${id}`);
         }
 
 
